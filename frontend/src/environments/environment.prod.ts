9c886491b0ea601f27cd3cdb565620048a87ae6f
SteMcLellan <mclellan.stephen@gmail.com>
2025-12-30 23:37:10 -0500

    fix api path and add rewrite rule

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://<production-function-app>.azurewebsites.net/api',
+  apiBaseUrl: '/api',
   googleClientId: '650542983574-na6dfg407lr33hgfrcr1m4hacg9t7dol.apps.googleusercontent.com'
 };
